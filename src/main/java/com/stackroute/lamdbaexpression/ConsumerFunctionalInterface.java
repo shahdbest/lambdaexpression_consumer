4ff28c88dbcc646682eed2bfdbc216500b56aed1
shahid <shahdbest@gmail.com>
2023-04-17 16:15:39 +0530

    solved

---
+++ src/main/java/com/stackroute/lamdbaexpression/ConsumerFunctionalInterface.java
@@ -0,0 +1,11 @@
+package com.stackroute.lamdbaexpression;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public class ConsumerFunctionalInterface {
+    //write logic to find the square of each number in a given list of numbers
+    public List<BigInteger> findSquare(List<BigInteger> list) {
+        return null;
+    }
+}
